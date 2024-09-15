86a8a71cc9eb00968a884b8570d254333d5acd72
Ian Dardik <idardik@msesrv6a-vm.mse.cs.cmu.edu>
2024-09-14 22:59:20 -0400

    Updating run_C1.sh for MSR for running in the bg.

--- examples/msr/naive_decomp/run_C1.sh
+++ examples/msr/naive_decomp/run_C1.sh
@@ -1,7 +1,7 @@
 #/bin/sh
 
-java -jar ../../../bin/assump-synth.jar MongoStaticRaft.tla MongoStaticRaft.cfg C1.tla MongoStaticRaft.cfg none \
+time java -jar ../../../bin/assump-synth.jar MongoStaticRaft.tla MongoStaticRaft.cfg C1.tla MongoStaticRaft.cfg none \
 C2.tla no_invs.cfg \
 C3.tla no_invs.cfg \
 C4.tla no_invs.cfg \
-T4.tla no_invs.cfg
+T4.tla no_invs.cfg >stdout.txt 2>stderr.txt
